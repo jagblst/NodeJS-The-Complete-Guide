627cc6d690f5f6eb00056b9e5980f3c10570ed32
Jaga <jagblst@gmail.com>
2024-01-09 16:39:50 +0200

    Express.js
    Filtering Paths

--- Basics/routes/admin.js
+++ Basics/routes/admin.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   res.send(`
-      <form action="/product" method="POST">
+      <form action="/admin/add-product" method="POST">
         <input type="text" name="title">
           <button type="submit">Add product</button>
         </input>
@@ -12,7 +12,7 @@ router.get("/add-product", (req, res, next) => {
     `);
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
