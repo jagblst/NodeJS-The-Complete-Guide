10b6f9b7e50b3aa8aead228b9a98c96121fc2cf2
Jaga <jagblst@gmail.com>
2024-01-16 16:39:48 +0200

    Sequelize
    Fetching Admin Products

--- SQL Database/util/database.js
+++ SQL Database/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'MyPassord', {
+const sequelize = new Sequelize('node-complete', 'root', 'MyPassword', {
   dialect: 'mysql',
   host: 'localhost',
 });
