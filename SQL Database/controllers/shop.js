48a7bea6c63ce4df1e0e994da3d2349d8546c135
Jaga <jagblst@gmail.com>
2024-01-17 16:51:12 +0200

    Sequelize
    Resetting the Cart & Fetching and Outputting Orders

--- SQL Database/controllers/shop.js
+++ SQL Database/controllers/shop.js
@@ -130,9 +130,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -146,7 +148,12 @@ exports.postOrder = (req, res, next) => {
             })
           );
         })
-        .then((result) => res.redirect('/orders'))
+        .then((result) => {
+          return fetchedCart.setProducts(null);
+        })
+        .then((result) => {
+          res.redirect('/orders');
+        })
         .catch((err) => console.log(err));
       console.log(products);
     })
@@ -154,15 +161,14 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders',
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout',
-  });
+  req.user
+    .getOrders({ include: ['products'] })
+    .then((orders) => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
