3ae9748225cb24541e03e7a0ec739a5ac9fb3e30
Jaga <jagblst@gmail.com>
2024-01-19 12:57:55 +0200

    MongoDB
    Displaying the Cart Items

--- MongoDB/controllers/shop.js
+++ MongoDB/controllers/shop.js
@@ -53,17 +53,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then((cart) => {
-      return cart
-        .getProducts()
-        .then((products) => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products,
-          });
-        })
-        .catch((err) => console.log(err));
+    .then((products) => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products,
+      });
     })
     .catch((err) => console.log(err));
 };
@@ -74,7 +69,10 @@ exports.postCart = (req, res, next) => {
     .then((product) => {
       return req.user.addToCart(product);
     })
-    .then((result) => console.log(result));
+    .then((result) => {
+      console.log(result);
+      res.redirect('/cart');
+    });
   // let fetchedCart;
   // let newQuantity = 1;
   // req.user
