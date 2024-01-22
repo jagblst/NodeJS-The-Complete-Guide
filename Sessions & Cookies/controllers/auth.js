4c644a22270f39b8da1d3f65e64da1097b855c01
Jaga <jagblst@gmail.com>
2024-01-22 13:37:50 +0200

    Sessions & Cookies
    Adding the Request Driven Login Solution

--- Sessions & Cookies/controllers/auth.js
+++ Sessions & Cookies/controllers/auth.js
@@ -3,6 +3,12 @@ exports.getLogin = (req, res, next) => {
     .render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
+      isAuthenticated: req.isLoggedIn,
     })
     .catch((err) => console.log(err));
+};
+
+exports.postLogin = (req, res, next) => {
+  req.isLoggedIn = true;
+  res.redirect('/');
 };
