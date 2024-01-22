3f112db8182f35e4a8fcc44dc83e77ab5066bd53
Jaga <jagblst@gmail.com>
2024-01-22 14:03:52 +0200

    Sessions & Cookies
    Setting a Cookie

--- Sessions & Cookies/controllers/auth.js
+++ Sessions & Cookies/controllers/auth.js
@@ -1,14 +1,13 @@
 exports.getLogin = (req, res, next) => {
-  res
-    .render('auth/login', {
-      path: '/login',
-      pageTitle: 'Login',
-      isAuthenticated: req.isLoggedIn,
-    })
-    .catch((err) => console.log(err));
+  const isLoggedIn = req.get('Cookie').trim().split('=')[1];
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: isLoggedIn,
+  });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  res.setHeader('Set-Cookie', 'loggedIn=true');
   res.redirect('/');
 };
