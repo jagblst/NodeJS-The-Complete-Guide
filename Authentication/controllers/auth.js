f01da1398684c298fc3c7312d5a967db837ec098
Jaga <jagblst@gmail.com>
2024-01-25 13:47:57 +0200

    Validation
    Adding Async Validation

--- Authentication/controllers/auth.js
+++ Authentication/controllers/auth.js
@@ -75,7 +75,6 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
@@ -85,34 +84,27 @@ exports.postSignup = (req, res, next) => {
       errorMessage: errors.array()[0].msg,
     });
   }
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash('error', 'Email already exists, please choose another one!');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] },
-          });
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect('/login');
-          return transporter.sendMail({
-            to: email,
-            from: 'shop@node-complete.com',
-            subject: 'Signup succeeded',
-            html: '<h1>You successfully signed up!</h1>',
-          });
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect('/login');
+      return transporter
+        .sendMail({
+          to: email,
+          from: 'shop@node-complete.com',
+          subject: 'Signup succeeded',
+          html: '<h1>You successfully signed up!</h1>',
         })
         .catch((err) => console.log(err));
-    })
-    .catch((err) => console.log(err));
+    });
 };
 
 exports.postLogout = (req, res, next) => {
--- Authentication/routes/auth.js
+++ Authentication/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -18,10 +19,17 @@ router.post(
       .isEmail()
       .withMessage('Please enter a valid email')
       .custom((value, { req }) => {
-        if (value === 'test@test.com') {
-          throw new Error('This email adress is forbidden!');
-        }
-        return true;
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email adress is forbidden!');
+        // }
+        // return true;
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              'Email already exists, please choose another one!'
+            );
+          }
+        });
       }),
     body(
       'password',
