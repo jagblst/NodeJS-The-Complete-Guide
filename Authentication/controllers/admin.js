09f02929008dd18f037eb528660c8026273364ce
Jaga <jagblst@gmail.com>
2024-01-26 13:26:59 +0200

    Error Handling
    Returning Error Pages

--- Authentication/controllers/admin.js
+++ Authentication/controllers/admin.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const { validationResult } = require('express-validator');
 const Product = require('../models/product');
 
@@ -24,7 +25,7 @@ exports.postAddProduct = (req, res, next) => {
   if (!errors.isEmpty()) {
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Add Product',
-      path: '/admin/edit-product',
+      path: '/admin/add-product',
       editing: false,
       hasError: true,
       product: {
@@ -39,6 +40,7 @@ exports.postAddProduct = (req, res, next) => {
   }
 
   const product = new Product({
+    _id: mongoose.Types.ObjectId('65aea3420cb326059f0fb9e9'),
     title: title,
     price: price,
     description: description,
@@ -53,7 +55,21 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect('/admin/products');
     })
     .catch((err) => {
-      console.log(err);
+      // return res.status(500).render('admin/edit-product', {
+      //   pageTitle: 'Add Product',
+      //   path: '/admin/add-product',
+      //   editing: false,
+      //   hasError: true,
+      //   product: {
+      //     title: title,
+      //     imageUrl: imageUrl,
+      //     price: price,
+      //     description: description,
+      //   },
+      //   errorMessage: 'Database operation failed!',
+      //   validationErrors: [],
+      // });
+      res.redirect('/500');
     });
 };
 
