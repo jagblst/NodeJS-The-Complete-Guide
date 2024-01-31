ebc1ca7ee4c2e68405fca2a463407419557d4fc0
Jaga <jagblst@gmail.com>
2024-01-31 16:14:46 +0200

    REST APIs - The Practical Application
    Setting Up a Post Model

--- REST API/app.js
+++ REST API/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -20,4 +21,9 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    'mongodb+srv://jagblst:67PlKjxXglodwrVI@cluster0.4q7kbiv.mongodb.net/messages'
+  )
+  .then((result) => app.listen(8080))
+  .catch((err) => console.log(err));
