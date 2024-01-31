c6fe95b7839d1b86a4c35d96a28b6b8b107846cb
Jaga <jagblst@gmail.com>
2024-01-31 16:40:06 +0200

    REST APIs - The Practical Application
    Static Images & Error Handling

--- REST API/app.js
+++ REST API/app.js
@@ -1,13 +1,17 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
+const { error } = require('console');
 
 const app = express();
 
 // app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,6 +24,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongoose
   .connect(
