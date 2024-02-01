bf9b5ef31ed6514443e14832a8a5bac3af35a214
Jaga <jagblst@gmail.com>
2024-02-01 13:36:03 +0200

    REST APIs - The Practical Application
    Updating Posts

--- REST API/app.js
+++ REST API/app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+// const cors = require('cors');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -37,6 +38,7 @@ app.use(
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+// app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
