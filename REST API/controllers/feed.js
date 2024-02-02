fa47093062dc154fb6360ec2cc341156b0fd0f33
Jaga <jagblst@gmail.com>
2024-02-02 16:48:07 +0200

    REST APIs - The Practical Application
    Clearing Post-User Relations

--- REST API/controllers/feed.js
+++ REST API/controllers/feed.js
@@ -167,9 +167,13 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndDelete(postId);
     })
     .then((result) => {
-      console.log(result);
-      res.status(200).json({ message: 'Post deleted!' });
+      return User.findById(req.userId);
     })
+    .then((user) => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then((result) => res.status(200).json({ message: 'Post deleted!' }))
     .catch((err) => {
       if (!err.statusCode) {
         err.statusCode = 500;
