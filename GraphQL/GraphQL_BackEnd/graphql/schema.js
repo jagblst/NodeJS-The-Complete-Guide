5f693a26191498ac70dd3c4c742189786a808f8f
Jaga <jagblst@gmail.com>
2024-02-07 14:09:39 +0200

    GraphQL
    Adding a "Get Post" Query & Resolver

--- GraphQL/GraphQL_BackEnd/graphql/schema.js
+++ GraphQL/GraphQL_BackEnd/graphql/schema.js
@@ -22,6 +22,10 @@ type AuthData {
     token: String!
     userId: String!
 }
+type PostData {
+    posts: [Post!]!
+    totalPosts: Int!
+}
 input UserInputData {
     email: String!
     name: String!
@@ -34,6 +38,7 @@ input PostInputData {
 }
 type RootQuery {
     login(email: String!, password: String!): AuthData!
+    posts: PostData!
 }
 type RootMutation {
     createUser(userInput: UserInputData): User!
